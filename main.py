4d2bd94e6eec0a388299bf65ebce40f0a35c4077
MagnusPDJ <magnusper@live.dk>
2024-09-03 20:55:07 +0200

    0.0.3: Updated code and added player sprite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting asteroids!")
@@ -9,13 +10,19 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        pygame.Surface.fill(screen, (0,0,0))
+        
+        screen.fill((0,0,0))
+        player.draw(screen)
+
         pygame.display.flip()
+
         dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
