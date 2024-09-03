317fdb80f57f93f4c898cb789b95c1505e5f021e
MagnusPDJ <magnusper@live.dk>
2024-09-03 23:07:39 +0200

    0.1.0: finished asteroid splitting when shot.

--- main.py
+++ main.py
@@ -43,9 +43,8 @@ def main():
                 exit()
             for shot in shots:
                 if asteroid.check_for_collision(shot):
-
                     shot.kill()
-                    asteroid.kill()
+                    asteroid.split()
 
         screen.fill((0,0,0))
 
