7c16d9cd7a32669b6522a379e7acdbd84dbd9900
MagnusPDJ <magnusper@live.dk>
2024-09-03 21:24:11 +0200

    0.0.6: added groups for game objects.

--- main.py
+++ main.py
@@ -10,18 +10,26 @@ def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
-    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-
-        player.update(dt)
+        
+        for object in updatable:
+            object.update(dt)
 
         screen.fill((0,0,0))
-        player.draw(screen)
+
+        for object in drawable:
+            object.draw(screen)
 
         pygame.display.flip()
 
