1e33552edc956b33feec14730531273c06b003cb
MagnusPDJ <magnusper@live.dk>
2024-09-05 19:09:59 +0200

    0.2.0 added scoring text that updatesduring play.

--- main.py
+++ main.py
@@ -14,29 +14,35 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids!")
     
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
     shots = pygame.sprite.Group()
-    
+
     Shot.containers = (updatable, drawable, shots)
     AsteroidField.containers = (updatable)
     Asteroid.containers = (updatable, drawable, asteroids)
+    Score.containers = (updatable, drawable)
+
     asteroidfield = AsteroidField()
     
     Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     score = Score()
+    print(score.get_score())
+    print(score.draw(screen))
 
-    while True:
+    while True: 
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        for object in updatable:
-            object.update(dt)
+        for update in updatable:
+            update.update(dt)
         
         for asteroid in asteroids:
             if asteroid.check_for_collision(player):
@@ -49,10 +55,10 @@ def main():
                     asteroid.split()
                     score.add_points(asteroid.radius)
 
-        screen.fill((0,0,0))
+        screen.fill((0,0,0))        
 
-        for object in drawable:
-            object.draw(screen)
+        for draw in drawable:
+            draw.draw(screen)
 
         pygame.display.flip()
 
--- score.py
+++ score.py
@@ -1,8 +1,17 @@
 import pygame
 
-class Score():
+class Score(pygame.sprite.Sprite):
     def __init__(self):
+        if hasattr(self, "containers"):
+            super().__init__(self.containers)
+        else:
+            super().__init__()   
+
         self.__score = 0
+        self.font = pygame.font.Font("freesansbold.ttf", 25)
+        self.text = self.font.render(f"Score: {self.get_score()}", True, "white")
+        self.__textrect = self.text.get_rect()
+        self.__textrect.center = (75, 30)
     
     def get_score(self):
         return self.__score
@@ -14,4 +23,8 @@ class Score():
             self.__score += 20
         if radius == 60:
             self.__score += 10
-
+    def draw(self, screen):
+        screen.blit(self.text, self.__textrect)
+    
+    def update(self, dt):
+        self.text = self.font.render(f"Score: {self.get_score()}", True, "white")
